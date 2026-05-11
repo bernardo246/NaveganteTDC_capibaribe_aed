e4efc98e0f5015b68217b2aa1d0151e07f43491b
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2026-05-10 23:29:21 -0300

    add mov por seta

--- lib/mecanica/movimentacao.c
+++ lib/mecanica/movimentacao.c
@@ -1,10 +1,10 @@
 #include "movimentacao.h"
 
 void mov_jogador(jogador *jogador){
-    if (IsKeyDown(KEY_W)) jogador->pos.y -= jogador->velocidade;
-    if (IsKeyDown(KEY_S)) jogador->pos.y += jogador->velocidade;
-    if (IsKeyDown(KEY_D)) jogador->pos.x += jogador->velocidade;
-    if (IsKeyDown(KEY_A)) jogador->pos.x -= jogador->velocidade;
+    if (IsKeyDown(KEY_W)|| IsKeyDown(KEY_UP)) jogador->pos.y -= jogador->velocidade;
+    if (IsKeyDown(KEY_S)|| IsKeyDown(KEY_DOWN)) jogador->pos.y += jogador->velocidade;
+    if (IsKeyDown(KEY_D)|| IsKeyDown(KEY_RIGHT)) jogador->pos.x += jogador->velocidade;
+    if (IsKeyDown(KEY_A)|| IsKeyDown(KEY_LEFT)) jogador->pos.x -= jogador->velocidade;
 
     if (jogador->pos.y > 720) jogador->pos.y = 720;
     if (jogador->pos.y < 0) jogador->pos.y = 0;
